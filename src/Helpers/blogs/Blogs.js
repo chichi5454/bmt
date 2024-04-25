b4b3e3a45df9c9ff926205770fd720494a193311
chichi5454 <140154706+chichi5454@users.noreply.github.com>
2024-04-25 14:49:54 +0300

    blogs

--- src/Helpers/blogs/Blogs.js
+++ src/Helpers/blogs/Blogs.js
@@ -2,12 +2,12 @@
 
 // import ifsl from "../../assets/blogs/ifsl.jpg";
 // import Patron1 from "../../../assets/events/Patron1.jpeg";
-// import BFA1 from "../../../assets/events/BFA1.jpeg";
+import BFA1 from "../../assets/blogs/BFA1.jpeg";
 import women1 from "../../assets/blogs/women1.jpeg";
-import Train1 from "../../assets/blogs/Train1.jpg";
+// import Train1 from "../../assets/blogs/Train1.jpg";
 import Vaccine1 from "../../assets/blogs/Vaccine1.jpg";
-import Fruit1 from "../../assets/blogs/Fruit1.jpg";
-import Beans1 from "../../assets/blogs/Beans1.png";
+// import Fruit1 from "../../assets/blogs/Fruit1.jpg";
+// import Beans1 from "../../assets/blogs/Beans1.png";
 // import Baby1 from "../../../assets/events/Baby1.jpeg";
 
 export const blogData = [
@@ -25,8 +25,8 @@ export const blogData = [
   },
   {
     id: 2,
-    image: Beans1,
-    title: "Agricultural Exchange Visit",
+    image: BFA1,
+    title: "Grassroot First Aid competitions",
     date: "1 February 2024",
     summary:
       "exposure to new agricultural technologies and innovations technologies for pest and disease management, soil fertility enhancement, mechanization, and precision agriculture will help farmers adopt more efficient and sustainable farming practices that will contribute to increased productivity and income generation.",
@@ -35,18 +35,30 @@ export const blogData = [
     des3: "Courtesy call to the Kenya Agricultural Live- stock and Research Organization also provided the farmers with valuable insights into the types, uses, and proper application of agro- chemicals.",
     author: "IFSL",
   },
-  {
-    id: 3,
-    image: Fruit1,
-    title: "Fruit Trees Distribution for Nutritional Enhancement",
-    date: "1 February 2024",
-    summary:
-      "Cultivation and nurturing of the trees like other fruit bearing trees will contribute to soil conser- vation and prevent erosion, thereby promoting overall health of the ecosystem.",
-    des1: "The project aiming to enhance the nutritional intake of households and mitigating deficiencies and improving overall health outcomes, Anti- FGM group based in Kamosiro, Youth Group in Kipsirat and two groups of Poultry Farmers located in Kaboson of Chebunyo ward have been supported with fruit trees.",
-    des2: "This diversification of agricultural practices helps buffer communities against the impacts of erratic weather patterns and changing growing conditions.",
-    des3: "The project not only addresses nutritional and environmental concerns but also fosters com- munity empowerment and cohesion, working collectively towards common goals of food security and sustainable livelihoods.",
-    author: "IFSL",
-  },
+  // {
+  //   id: 2,
+  //   image: Beans1,
+  //   title: "Agricultural Exchange Visit",
+  //   date: "1 February 2024",
+  //   summary:
+  //     "exposure to new agricultural technologies and innovations technologies for pest and disease management, soil fertility enhancement, mechanization, and precision agriculture will help farmers adopt more efficient and sustainable farming practices that will contribute to increased productivity and income generation.",
+  //   des1: "Farmers supported by the project attended an exchange visit at Utangwa Irrigation scheme in Makueni County. This provided valuable opportunities and enhanced the farmer's knowledge on best irrigation management practices, fruit rearing and grafting skills to help them diversify their agricultural activities and increase their income potential.",
+  //   des2: "They were also taken through crop and animal sexting to help in making informed decisions regarding breeding programs, selection of superior genetic traits and management of breeding populations.",
+  //   des3: "Courtesy call to the Kenya Agricultural Live- stock and Research Organization also provided the farmers with valuable insights into the types, uses, and proper application of agro- chemicals.",
+  //   author: "IFSL",
+  // },
+  // {
+  //   id: 3,
+  //   image: Fruit1,
+  //   title: "Fruit Trees Distribution for Nutritional Enhancement",
+  //   date: "1 February 2024",
+  //   summary:
+  //     "Cultivation and nurturing of the trees like other fruit bearing trees will contribute to soil conser- vation and prevent erosion, thereby promoting overall health of the ecosystem.",
+  //   des1: "The project aiming to enhance the nutritional intake of households and mitigating deficiencies and improving overall health outcomes, Anti- FGM group based in Kamosiro, Youth Group in Kipsirat and two groups of Poultry Farmers located in Kaboson of Chebunyo ward have been supported with fruit trees.",
+  //   des2: "This diversification of agricultural practices helps buffer communities against the impacts of erratic weather patterns and changing growing conditions.",
+  //   des3: "The project not only addresses nutritional and environmental concerns but also fosters com- munity empowerment and cohesion, working collectively towards common goals of food security and sustainable livelihoods.",
+  //   author: "IFSL",
+  // },
   {
     id: 4,
     image: Vaccine1,
@@ -59,16 +71,16 @@ export const blogData = [
     des3: "Prior to distribution of the chicks to the targeted 50 households, farmers were trained on best animal husbandry practices, the importance of disease prevention measures and vaccination schedules, empowering them with knowledge to better manage their poultry enterprises in the long term.",
     author: "IFSL",
   },
-  {
-    id: 5,
-    image: Train1,
-    title: "High Iron Bean Training Program",
-    date: "1 February 2024",
-    summary:
-      "The training emphasized on the safe and responsible use of agrochemicals, including proper application methods, dosage, and timing, to maximize their effectiveness",
-    des1: "In collaboration with Kenya Agricultural and Livestock Research Organization farmers drawn from block 8 and 9 in Nogirwet Irrigation Scheme were trained on High Iron Bean varieties with specific focus on Angaza beans.",
-    des2: "The training sought to raise awareness among participants, including farmers and agricultural officers about the significance of micronutrient beans in improving nutrition, health, and house- hold income hence promoting adoption of these varieties based on the characteristics, agronomic requirements and market demand.",
-    des3: "Farmers were also equipped with knowledge and skills on pest and disease identification symptoms, integrated pest management (IPM) strategies and environmentally friendly approaches to control these threats to bean production.",
-    author: "IFSL",
-  },
+  // {
+  //   id: 5,
+  //   image: Train1,
+  //   title: "High Iron Bean Training Program",
+  //   date: "1 February 2024",
+  //   summary:
+  //     "The training emphasized on the safe and responsible use of agrochemicals, including proper application methods, dosage, and timing, to maximize their effectiveness",
+  //   des1: "In collaboration with Kenya Agricultural and Livestock Research Organization farmers drawn from block 8 and 9 in Nogirwet Irrigation Scheme were trained on High Iron Bean varieties with specific focus on Angaza beans.",
+  //   des2: "The training sought to raise awareness among participants, including farmers and agricultural officers about the significance of micronutrient beans in improving nutrition, health, and house- hold income hence promoting adoption of these varieties based on the characteristics, agronomic requirements and market demand.",
+  //   des3: "Farmers were also equipped with knowledge and skills on pest and disease identification symptoms, integrated pest management (IPM) strategies and environmentally friendly approaches to control these threats to bean production.",
+  //   author: "IFSL",
+  // },
 ];
