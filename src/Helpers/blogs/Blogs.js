664a6f3aa8bf614671001be2097dfe672c544e49
laurynM365Connect <lauryn.m@365connect.com>
2024-05-23 15:27:02 +0300

    blogs 404

--- src/Helpers/blogs/Blogs.js
+++ src/Helpers/blogs/Blogs.js
@@ -18,11 +18,11 @@ export const blogData = [
     title: "Annual General meeting KRCS Bomet Branch",
     date: "27th April 2024",
     summary:
-      "KRCS Bomet branch held the Annual General meeting at Bomet University.",
+      "The Annual General meeting of the Branch was held at Bomet University.",
     des1: "The meeting geared towards recognizing the tremendous efforts of the members in ensuring that the branch operates effectively.Volunteers were also awarded and recognized in different capacities for their dedication and devotion in alleviating human suffering",
     des2: "The new Head of Region, in attendance, applauded members for their unwavering support to the branch, urging them to keep up with the good work.",
     des3: "The HOR later addressed the media on the situation in the region, with specific focus on the ongoing rains, noting different mechanisms put in place to ensure the affected populations are offered the required support including psychosocial support.",
-    author: "IFSL",
+    author:"PR Team",
   },
   {
     id: 1,
@@ -31,10 +31,10 @@ export const blogData = [
     date: "1 February 2024",
     summary:
       "Recognizing and celebrating the contributions of women to agriculture on International Women's Day highlights their importance in food production and underscores the need to support their efforts to ensure food security for their families and communities. ",
-    des1: "International Women's Day is a global celebration of women's achievements and a call to action for gender equality. The project demonstrates its commitment to promoting gender equality and empowering women in the community through recognizing their contributions to food security and livelihoods, advocating for their rights and addressing gender-based barriers they may face in accessing resources and opportunities.",
+    des1: "International Women's Day is a global celebration of women's achievements and a call to action for gender equality. The  branch demonstrates its commitment to promoting gender equality and empowering women in the community through recognizing their contributions to food security and livelihoods, advocating for their rights and addressing gender-based barriers they may face in accessing resources and opportunities.",
     des2: "Through community dialogues and review meetings, the project champions for community and advocacy on topics such as women's rights, gender-based violence and the importance of gender-responsive approaches to development.",
     des3: "It also fosters greater understanding and support for gender equality initiatives leveraging the essentiality of collective efforts to advance gender equality and promoting women's rights within the community in line with this year's theme, - Invest in Women, Accelerate change",
-    author: "IFSL",
+    author:"PR Team",
   },
   {
     id: 2,
@@ -43,9 +43,9 @@ export const blogData = [
     date: "1 February 2024",
     summary:
       "The competition simulated real life scenarios forcing participants to act swiftly and make rapid decisions that are essential in saving lives.",
-    des1: "KRCS Bomet branch conducted Grassroot First Aid competitions that brought together 30 schools, 7 Chapters 3 Corporates, branch Staff and Red Cross Action Team.",
+    des1: "The  branch conducted Grassroot First Aid competitions that brought together 30 schools, 7 Chapters 3 Corporates, branch Staff and Red Cross Action Team.",
     des2: "The top two teams proceed to the Regional Competitions to be held in Nyandarua County, on the 8th of June 2024",
-    author: "IFSL",
+    author:"PR Team",
   },
   // {
   //   id: 2,
@@ -79,19 +79,19 @@ export const blogData = [
     summary:
       "Patrons play an essential role in the daily operations of the branch and bringing them on board ensures that decisions made lead to better outcomes for both the branch and the schools.",
     des1: "The  branch held patrons meeting to reflect on last years events and deliberate on this years plans with specific focus on Humanity Power Walk, Grassroots First aid Competition and the best mechanisms that the branch can undertake in realization of several activities in the society",
-    author: "KRCS-Bomet Branch",
+    author:"PR Team",
   },
   {
     id: 4,
     image: Vaccine1,
-    title: "Poultry Vaccination Initiative",
+    title: "Poultry Vaccination ",
     date: "1 February 2024",
     summary:
       "Each of the 50 households have been supported with 50 improved kienyeji chicks.",
     des1: " In partnership with the county government of Bomet, department of Agriculture Livestock, Fisheries and Cooperatives vaccination exercise against fowl pox to improved Kieyeji Chicks was conducted at Kaboson Location, Chebunyo ward of Chepalungu Sub County, a crucial initi- ative for the health and welfare of poultry.",
     des2: "Fowl Pox is a highly contagious viral disease that affects chickens, causing significant econo- mic losses due to decreased egg production, poor growth rates, and sometimes mortality. The project aims at maintaining the health and productivity of poultry, thereby safeguarding the livelihoods of households.",
     des3: "Prior to distribution of the chicks to the targeted 50 households, farmers were trained on best animal husbandry practices, the importance of disease prevention measures and vaccination schedules, empowering them with knowledge to better manage their poultry enterprises in the long term.",
-    author: "IFSL",
+    author:"PR Team",
   },
 
   // {
